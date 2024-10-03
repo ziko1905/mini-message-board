c17268e4dc96bfba803a90a5af15c17e4987ee4a
Karlo Cehulic <karlocehlic@gmail.com>
2024-10-03 15:55:24 +0200

    New msg page working properly(in progress)

--- app.js
+++ app.js
@@ -6,17 +6,31 @@ const PORT = 3000;
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 
+const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    }
+  ];
+  
+
 app.get("/", (req, res) => {
     res.render("index", {pageTitle: "Mini Messanger"})
     // res.send("Homepage send")
 })
 
 app.get("/new", () => {
-    app.render("new", {pageTitle: "New Message"})
+    app.render("new", {pageTitle: "New Message", messages})
 })
 
 app.use((err, req, res, next) => {
-    res.stauts(500).render("error", { errMsg: err })
+    res.render("error", { errMsg: err })
     console.log(err)
 })
 
