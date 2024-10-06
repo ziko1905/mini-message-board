edd8be30652a53479162083ca9b7c8430bf2dc59
Karlo Cehulic <karlocehlic@gmail.com>
2024-10-06 04:13:20 +0200

    Console log public url

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,6 @@
 const { Pool } = require("pg")
 
+console.log(process.env.DATABASE_PUBLIC_URL, "!DATABASE_PUBLIC_URL!")
 module.exports = new Pool({
     connectionString: process.env.DATABASE_PUBLIC_URL
 })
