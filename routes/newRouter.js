10fd7a887d89e4d1f478428c4326013f12ad81df
Karlo Cehulic <karlocehlic@gmail.com>
2024-10-06 02:00:24 +0200

    Implement user cration with db

--- routes/newRouter.js
+++ routes/newRouter.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const router = express.Router()
+const db = require("../db/queries")
 
 router.get("/new", (req, res) => {
     res.render("form", {pageTitle: "New Message"})
 })
 
-router.post("/new", (req, res, next) => {
-    "need to insert to db - wip"
+router.post("/new", async (req, res, next) => {
+    await db.createMessage(req.body)
     res.redirect('/')
 })
 
